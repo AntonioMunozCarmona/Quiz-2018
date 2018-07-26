c7f4f8fb15a69f335f6c406125c193ce0afda517
Antonio José Muñoz Carmona <amcarmona@telefonica.net>
2018-07-26 12:11:03 +0200

    Step 2 - Title, favicon, images, remove Users

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* Author page*/
+router.get('/author', function(req, res, next) {
+  res.render('author', { nombre: 'Antonio', ape: 'Muñoz' });
+});
 
 module.exports = router;
