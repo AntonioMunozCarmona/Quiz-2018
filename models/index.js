bbf3f0175e3ebdf5891209921b37320f3e461fbd
Antonio José Muñoz Carmona <amcarmona@telefonica.net>
2018-10-01 19:23:46 +0200

    Step 10 - Despliegue en Heroku

--- models/index.js
+++ models/index.js
@@ -3,8 +3,11 @@ const path = require('path');
 // Load ORM
 const Sequelize = require('sequelize');
 
+// Configurar url a sQlite o Progress
+const url = process.env.DATABASE_URL || 'sqlite:quiz.sqlite';
+
 // Para usar la Base de Datos SQLite
-const sequelize = new Sequelize('sqlite:quiz.sqlite');
+const sequelize = new Sequelize(url);
 
 // Para importar la definición de la tabla quiz desde quiz.js
 sequelize.import(path.join(__dirname, 'quiz'));
